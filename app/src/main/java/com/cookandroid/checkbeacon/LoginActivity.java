b1c12538664edec811f99831ac97d09954186661
HAEUN <lhe4352@naver.com>
2017-12-14 17:29:10 +0900

    Initial commit

---
+++ app/src/main/java/com/cookandroid/checkbeacon/LoginActivity.java
@@ -0,0 +1,172 @@
+package com.cookandroid.checkbeacon;
+
+import android.Manifest;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothManager;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
+
+public class LoginActivity extends AppCompatActivity {
+
+    public static final boolean ENABLE_BACKGROUND_RANGING_TIMEOUT = true;
+
+    /**
+     * DISCONTINUOUS_SCAN:
+     * <p/>
+     * There is a known android bug that some android devices scan BLE devices only once.
+     * (link: http://code.google.com/p/android/issues/detail?id=65863)
+     * To resolve the bug in our SDK, you can use setDiscontinuousScan() method of the RECOBeaconManager.
+     * This method is to set whether the device scans BLE devices continuously or discontinuously.
+     * The default is set as FALSE. Please set TRUE only for specific devices.
+     * <p/>
+     * 일부 안드로이드 기기에서 BLE 장치들을 스캔할 때, 한 번만 스캔 후 스캔하지 않는 버그(참고: http://code.google.com/p/android/issues/detail?id=65863)가 있습니다.
+     * 해당 버그를 SDK에서 해결하기 위해, RECOBeaconManager에 setDiscontinuousScan() 메소드를 이용할 수 있습니다.
+     * 해당 메소드는 기기에서 BLE 장치들을 스캔할 때(즉, ranging 시에), 연속적으로 계속 스캔할 것인지, 불연속적으로 스캔할 것인지 설정하는 것입니다.
+     * 기본 값은 FALSE로 설정되어 있으며, 특정 장치에 대해 TRUE로 설정하시길 권장합니다.
+     */
+    public static final boolean DISCONTINUOUS_SCAN = false;
+
+    private static final int REQUEST_ENABLE_BT = 1;
+    private static final int REQUEST_LOCATION = 10;
+
+    private BluetoothManager mBluetoothManager;
+    private BluetoothAdapter mBluetoothAdapter;
+
+    private View mLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        mLayout = findViewById(R.id.loginActivity);
+
+        //If a user device turns off bluetooth, request to turn it on.
+        //사용자가 블루투스를 켜도록 요청합니다.
+        mBluetoothManager = (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
+        mBluetoothAdapter = mBluetoothManager.getAdapter();
+
+        if (mBluetoothAdapter == null || !mBluetoothAdapter.isEnabled()) {
+            Intent enableBTIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBTIntent, REQUEST_ENABLE_BT);
+        }
+
+        /**
+         * In order to use RECO SDK for Android API 23 (Marshmallow) or higher,
+         * the location permission (ACCESS_COARSE_LOCATION or ACCESS_FINE_LOCATION) is required.
+         * Please refer to the following permission guide and sample code provided by Google.
+         *
+         * 안드로이드 API 23 (마시멜로우)이상 버전부터, 정상적으로 RECO SDK를 사용하기 위해서는
+         * 위치 권한 (ACCESS_COARSE_LOCATION 혹은 ACCESS_FINE_LOCATION)을 요청해야 합니다.
+         * 권한 요청의 경우, 구글에서 제공하는 가이드를 참고하시기 바랍니다.
+         *
+         * http://www.google.com/design/spec/patterns/permissions.html
+         * https://github.com/googlesamples/android-RuntimePermissions
+         */
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                Log.i("LoginActivity", "The location permission (ACCESS_COARSE_LOCATION or ACCESS_FINE_LOCATION) is not granted.");
+                this.requestLocationPermission();
+            } else {
+                Log.i("LoginActivity", "The location permission (ACCESS_COARSE_LOCATION or ACCESS_FINE_LOCATION) is already granted.");
+            }
+        }
+
+        final EditText idText = (EditText) findViewById(R.id.idText);
+        final EditText passwordText = (EditText) findViewById(R.id.passwordText);
+
+        final Button loginButton = (Button) findViewById(R.id.loginButton);
+        final TextView registerButton = (TextView) findViewById(R.id.registerButton);
+
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent registerIntent = new Intent(LoginActivity.this, RegisterActivity.class);
+                LoginActivity.this.startActivity(registerIntent);
+            }
+        });
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final String userID = idText.getText().toString();
+                final String userPassword = passwordText.getText().toString();
+
+                Response.Listener<String> responseListener = new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject jsonResponse = new JSONObject(response);
+                            boolean success = jsonResponse.getBoolean("success");
+                            if (success) {
+                                String userID = jsonResponse.getString("userID");
+                                String userPassword = jsonResponse.getString("userPassword");
+                                String userName = jsonResponse.getString("userName");
+
+                                Intent intent = new Intent(LoginActivity.this, RecoBackgroundMonitoringService.class);
+                                intent.putExtra("userID", userID);
+                                intent.putExtra("userName", userName);
+                                startService(intent);
+
+                                Intent intent2 = new Intent(LoginActivity.this, MainActivity.class);
+                                intent2.putExtra("userID", userID);
+                                intent2.putExtra("userPassword", userPassword);
+                                intent2.putExtra("userName", userName);
+                                LoginActivity.this.startActivity(intent2);
+                            }
+                            else {
+                                AlertDialog.Builder builder = new AlertDialog.Builder(LoginActivity.this);
+                                builder.setMessage("로그인에 실패하였습니다.")
+                                        .setNegativeButton("다시 시도", null)
+                                        .create()
+                                        .show();
+                            }
+                        }
+                        catch(Exception e)
+                        {
+                            e.printStackTrace();
+                        }
+                    }
+                };
+
+                LoginRequest loginRequest = new LoginRequest(userID, userPassword, responseListener);
+                RequestQueue queue = Volley.newRequestQueue(LoginActivity.this);
+                queue.add(loginRequest);
+            }
+        });
+
+    }
+
+    private void requestLocationPermission() {
+        if (!ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_COARSE_LOCATION)) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, REQUEST_LOCATION);
+            return;
+        }
+
+        Snackbar.make(mLayout, R.string.location_permission_rationale, Snackbar.LENGTH_INDEFINITE)
+                .setAction(R.string.ok, new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        ActivityCompat.requestPermissions(LoginActivity.this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, REQUEST_LOCATION);
+                    }
+                })
+                .show();
+    }
+}
